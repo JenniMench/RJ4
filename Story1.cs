ed28eb64f3d937f1059836600a99e2d5c15f1fa3
miu <jenny.lovechimera21@gmail.com>
2013-11-13 11:26:04 +0800

    version 1.0
    not yet reviewed by code reviewer

---
+++ Story1.cs
@@ -0,0 +1,162 @@
+using UnityEngine;
+using System.Collections;
+
+public class Story1 : MonoBehaviour {
+
+	public GUISkin guiSkin;
+	public GUISkin guiSkin2;
+	public Texture2D background;
+	public Texture2D background2;
+	public Texture2D background3;
+	public TextAsset storyFile;
+	public Texture arrowHead;
+	public Texture hKing;
+	public Texture hPrince;
+	public Texture hSoldier;
+	public Texture fatMerchant;
+	
+    string[] storyLines1;
+	int ctr = 0;
+    void Start () {
+         storyLines1 = ( storyFile.text.Split( '\n' ) );
+    }
+
+	void Update () {
+		 if (Input.GetKeyDown("return")) {
+           	ctr++;
+		}
+	}
+	
+	private void OnGUI() {
+		if (background != null && ctr <= 15) {
+			GUI.skin = guiSkin;
+	        GUI.DrawTexture(new Rect(0,0,Screen.width , Screen.height),background);
+		}
+		else if(ctr >=17 && ctr < 21) {
+			GUI.skin = guiSkin;
+			GUI.DrawTexture(new Rect(0,0,Screen.width , Screen.height),background2);
+		}
+		else {
+			GUI.skin = guiSkin2;
+			GUI.DrawTexture(new Rect(0,0,Screen.width , Screen.height),background3);
+		}
+		
+		string dialog = storyLines1[ctr];;
+		
+		switch(ctr){
+			case 0:
+				GUI.DrawTexture(new Rect(20,120,200,400),hKing);
+				GUI.Box (new Rect (0,Screen.height - 250,600,250), dialog);	
+				GUI.DrawTexture(new Rect(520,530,37,32),arrowHead);
+				break;
+			case 1:
+				GUI.DrawTexture(new Rect(630,150,140,300),hPrince);
+				GUI.Box (new Rect (Screen.width - 600,Screen.height - 250,600,250), dialog);
+				GUI.DrawTexture(new Rect(720, 530,37,32),arrowHead);
+				break;
+			case 2:
+				GUI.DrawTexture(new Rect(20,120,200,400),hKing);
+				GUI.Box (new Rect (0,Screen.height - 250,600,250), dialog);	
+				GUI.DrawTexture(new Rect(520,530,37,32),arrowHead);
+				break;
+			case 3:
+				GUI.DrawTexture(new Rect(20,120,200,400),hKing);
+				GUI.Box (new Rect (0,Screen.height - 250,600,250), dialog);	
+				GUI.DrawTexture(new Rect(520,530,37,32),arrowHead);
+				break;
+			case 4:
+				GUI.DrawTexture(new Rect(630,150,140,300),hPrince);
+				GUI.Box (new Rect (Screen.width - 600,Screen.height - 250,600,250), dialog);
+				GUI.DrawTexture(new Rect(720, 530,37,32),arrowHead);
+				break;
+			case 5:
+				GUI.DrawTexture(new Rect(20,120,200,400),hKing);
+				GUI.Box (new Rect (0,Screen.height - 250,600,250), dialog);	
+				GUI.DrawTexture(new Rect(520,530,37,32),arrowHead);
+				break;
+			case 6:
+				GUI.DrawTexture(new Rect(630,150,140,300),hPrince);
+				GUI.Box (new Rect (Screen.width - 600,Screen.height - 250,600,250), dialog);
+				GUI.DrawTexture(new Rect(720, 530,37,32),arrowHead);
+				break;
+			case 7:
+				GUI.DrawTexture(new Rect(20,120,200,400),hKing);
+				GUI.Box (new Rect (0,Screen.height - 250,600,250), dialog);	
+				GUI.DrawTexture(new Rect(520,530,37,32),arrowHead);
+				break;
+			case 8:
+				GUI.DrawTexture(new Rect(20,120,200,400),hKing);
+				GUI.Box (new Rect (0,Screen.height - 250,600,250), dialog);	
+				GUI.DrawTexture(new Rect(520,530,37,32),arrowHead);
+				break;
+			case 9:
+				GUI.DrawTexture(new Rect(630,150,140,300),hPrince);
+				GUI.Box (new Rect (Screen.width - 600,Screen.height - 250,600,250), dialog);
+				GUI.DrawTexture(new Rect(720, 530,37,32),arrowHead);
+				break;
+			case 10:
+				GUI.DrawTexture(new Rect(20,120,200,400),hKing);
+				GUI.Box (new Rect (0,Screen.height - 250,600,250), dialog);	
+				GUI.DrawTexture(new Rect(520,530,37,32),arrowHead);
+				break;
+			case 11:
+				GUI.DrawTexture(new Rect(630,150,140,300),hPrince);
+				GUI.Box (new Rect (Screen.width - 600,Screen.height - 250,600,250), dialog);
+				GUI.DrawTexture(new Rect(720, 530,37,32),arrowHead);
+				break;
+			case 12:
+				GUI.DrawTexture(new Rect(20,120,200,400),hSoldier);
+				GUI.Box (new Rect (0,Screen.height - 250,600,250), dialog);	
+				GUI.DrawTexture(new Rect(520,530,45,32),arrowHead);
+				break;
+			case 13:
+				GUI.DrawTexture(new Rect(20,120,200,400),hKing);
+				GUI.Box (new Rect (0,Screen.height - 250,600,250), dialog);	
+				GUI.DrawTexture(new Rect(520,530,37,32),arrowHead);
+				break;
+			case 14:
+				GUI.DrawTexture(new Rect(630,150,140,300),hPrince);
+				GUI.Box (new Rect (Screen.width - 600,Screen.height - 250,600,250), dialog);
+				GUI.DrawTexture(new Rect(720, 530,37,32),arrowHead);
+				break;
+			case 15:
+				GUI.DrawTexture(new Rect(20,120,200,400),hKing);
+				GUI.Box (new Rect (0,Screen.height - 250,600,250), dialog);	
+				GUI.DrawTexture(new Rect(520,530,37,32),arrowHead);
+				break;
+			case 16:
+				GUI.Box (new Rect (100,20,600,550), dialog );	
+				GUI.DrawTexture(new Rect(610,470,37,32),arrowHead);
+				break;
+			case 17:
+				GUI.DrawTexture(new Rect(20,120,200,400),fatMerchant);
+				GUI.Box (new Rect (0,Screen.height - 250,600,250), dialog);	
+				GUI.DrawTexture(new Rect(520,530,37,32),arrowHead);
+				break;
+			case 18:
+				GUI.DrawTexture(new Rect(630,150,140,300),hPrince);
+				GUI.Box (new Rect (Screen.width - 600,Screen.height - 250,600,250), dialog);
+				GUI.DrawTexture(new Rect(720, 530,37,32),arrowHead);
+				break;
+			case 19:
+				GUI.DrawTexture(new Rect(20,120,200,400),fatMerchant);
+				GUI.Box (new Rect (0,Screen.height - 250,600,250), dialog);	
+				GUI.DrawTexture(new Rect(520,530,37,32),arrowHead);
+				break;
+			case 20:
+				GUI.DrawTexture(new Rect(630,150,140,300),hPrince);
+				GUI.Box (new Rect (Screen.width - 600,Screen.height - 250,600,250), dialog);
+				GUI.DrawTexture(new Rect(720, 530,37,32),arrowHead);
+				break;
+			case 21:
+				GUI.Box (new Rect (100,20,600,550), dialog );	
+				GUI.DrawTexture(new Rect(610,470,37,32),arrowHead);
+				break;
+			default:
+                Debug.Log("pig");
+                break;  
+			}
+		
+		}
+	}
+
